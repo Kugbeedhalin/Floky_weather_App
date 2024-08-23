f77fd57a2d154113a07cf4b82796409626869694
Kugbeedhalin <49689060+Kugbeedhalin@users.noreply.github.com>
2024-08-23 14:40:14 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+const apikey = "7eb0eabf406227f6ec33aba5b95e5e65";
+const apiurl = "https://api.openweathermap.org/data/2.5/weather";
+
+const locationInput = document.getElementById("locationInput");
+const searchButton = document.getElementById("searchButton");
+const locationElement = document.getElementById("location");
+const temperatureElement = document.getElementById("temperature");
+const descriptionElement = document.getElementById("description");
+
+
+searchButton.addEventListener('click', () => {
+  const location = locationInput.value ;
+  if (location){
+    fetchweather(location);
+  }
+});
+
+function fetchweather(location){
+  const url = `${apiurl}?q=${location}&appid=${apikey}&units=metric`;
+
+  fetch(url)
+      .then(response => response.json())
+      .then(data =>{
+        locationElement.textContent = data.name;
+        temperatureElement.textContent = `${Math.round(data.main.temp)}°C`;
+        descriptionElement.textContent = data.weather[0].description;
+      })
+      .catch(error => {
+        console.error("Error Fetching weather data: ", error);
+      });
+}
